d078d58f53e9424f278ab61b23d014baad5b2704
SpotlightForBugs <mail@spotlightforbugs.eu>
2022-08-22 18:01:00 +0200

    redone sth

--- generate.py
+++ generate.py
@@ -3,8 +3,23 @@
 #wordlist generator 
     
 
-import Faker
+from faker import Faker
 
 
+def clear_file():
+    with open("text.txt", 'w') as f:
+        f.write('')
+    print('Done')
+    
+    
+
+def fake_name_wordlist_in_file():
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(100):
+            f.write(fake.name() + '\n')
+    print('Done')
+    
+    
 
 
