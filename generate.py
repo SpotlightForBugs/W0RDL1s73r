a1fc7fdd39faa735b8f46fc538fdf2c544e5c25d
SpotlightForBugs <mail@spotlightforbugs.eu>
2022-08-22 18:22:14 +0200

    smth

--- generate.py
+++ generate.py
@@ -1,25 +1,113 @@
 #!/usr/bin/env python3
 
-#wordlist generator 
-    
+# wordlist generator
+
 
 from faker import Faker
+from faker.providers import internet
 
 
 def clear_file():
     with open("text.txt", 'w') as f:
         f.write('')
     print('Done')
-    
-    
 
-def fake_name_wordlist_in_file():
+
+def fake_name_wordlist(number):
+    clear_file()
     fake = Faker()
     with open("text.txt", 'w') as f:
-        for i in range(100):
+        for i in range(number):
             f.write(fake.name() + '\n')
     print('Done')
-    
-    
 
+
+def fake_free_email_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.free_email() + '\n')
+    print('Done')
+
+
+def fake_net_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.ipv4() + '\n')
+    print('Done')
+
+
+def fake_phone_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.phone_number() + '\n')
+    print('Done')
+
+
+def fake_MAC_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.mac_address() + '\n')
+    print('Done')
+
+
+def fake_password_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.password() + '\n')
+    print('Done')
+
+
+def fake_address_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.address() + '\n')
+    print('Done')
+
+
+def fake_job_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.job() + '\n')
+    print('Done')
+
+
+def fake_text_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.text() + '\n')
+    print('Done')
+
+
+def fake_sentence_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.sentence() + '\n')
+    print('Done')
+
+
+def fake_paragraph_wordlist(number):
+    clear_file()
+    fake = Faker()
+    with open("text.txt", 'w') as f:
+        for i in range(number):
+            f.write(fake.paragraph() + '\n')
+    print('Done')
 
